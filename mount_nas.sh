522063a94950244e7f77d9c45fee796bb2f416f9
Michael König <koenigmichael2002@gmail.com>
2019-12-07 19:43:59 +0100

    Fix files

--- mount_nas.sh
+++ mount_nas.sh
@@ -1,1 +1,2 @@
-/home/michael/mount_nas.sh
+mount -F 10.0.0.33:/volume1/homes /mnt/nas/homes
+mount -F 10.0.0.33:/volume1/Videos /mnt/nas/videos
--- start_prometheus_grafana.sh
+++ start_prometheus_grafana.sh
@@ -1,1 +1,5 @@
-/home/michael/start_prometheus_grafana.sh
+cd /usr/bin/node_exporter-0.18.1.linux-amd64
+./node_exporter &
+cd /usr/bin/prometheus-2.14.0.linux-amd64
+./prometheus &
+systemctl start grafana-server &

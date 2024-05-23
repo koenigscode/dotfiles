bdf74021b83cf0ffe5a2cf99fd85ad0edcb9854b
Michael Koenig <koenigmichael2002@gmail.com>
2024-05-23 18:29:38 +0200

    Improve nvim suggestions, use telescope for UI actions

--- nvim/lua/koenigscode/plugins/lsp.lua
+++ nvim/lua/koenigscode/plugins/lsp.lua
@@ -13,8 +13,8 @@ return {
             vim.g.lsp_zero_gopls = {
                 settings = {
                     gopls = {
-            completeUnimported = true,
-                },
+                        completeUnimported = true,
+                    },
                 }
             }
         end,
@@ -30,7 +30,12 @@ return {
         'hrsh7th/nvim-cmp',
         event = 'InsertEnter',
         dependencies = {
-            { 'L3MON4D3/LuaSnip' },
+            { 'L3MON4D3/LuaSnip',
+                "onsails/lspkind.nvim",
+                "hrsh7th/cmp-nvim-lsp",
+                "hrsh7th/cmp-path",
+                "hrsh7th/cmp-buffer",
+            },
         },
         config = function()
             -- Here is where you configure the autocompletion settings.
--- nvim/lua/koenigscode/plugins/telescope.lua
+++ nvim/lua/koenigscode/plugins/telescope.lua
@@ -3,13 +3,9 @@ return {
     branch = '0.1.x',
     dependencies = {
         'nvim-lua/plenary.nvim',
-        -- Fuzzy Finder Algorithm which requires local dependencies to be built.
-        -- Only load if `make` is available. Make sure you have the system
-        -- requirements installed.
+        "nvim-telescope/telescope-ui-select.nvim",
         {
             'nvim-telescope/telescope-fzf-native.nvim',
-            -- NOTE: If you are having trouble with this installation,
-            --       refer to the README for telescope-fzf-native for more instructions.
             build = 'make',
             cond = function()
                 return vim.fn.executable 'make' == 1
@@ -17,8 +13,19 @@ return {
         },
     },
     config = function()
-        -- bunch of this is taken from kickstart.nvim/init.lua
+        require("telescope").setup {
+            extensions = {
+                ["ui-select"] = {
+                    require("telescope.themes").get_dropdown {
+                    }
+                }
+            }
+        }
 
+        -- Load ui-select extension
+        require("telescope").load_extension("ui-select")
+
+        -- Keymap configurations
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = "[S]earch [F]iles" })
         vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = "[S]earch [G]rep" })
@@ -27,6 +34,7 @@ return {
         vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
         vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
         vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+        vim.keymap.set('n', 'gd', builtin.lsp_definitions, { desc = '[G]oto [D]efinition' })
 
         vim.keymap.set('n', '<leader>/', function()
             builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
@@ -40,3 +48,4 @@ return {
         end, { desc = '[S]earch [N]eovim files' })
     end,
 }
+
